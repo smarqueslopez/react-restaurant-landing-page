66c08dfbde80a0d7ecf2bb1759dcedc50b39b445
smarqueslopez <smarqueslopez@gmail.com>
2021-10-05 17:13:45 +0200

    feat(global): :sparkles: Adding header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,67 @@
+import classNames from 'classnames'
+import { useState } from 'react'
 import styles from '@/styles/components/Header.module.scss'
 
 function Header() {
-  return <header className={styles.header}></header>
+  const [toggle, setToggle] = useState(false)
+
+  const onMenuClick = (e) => {
+    setToggle(!toggle)
+    const menuIcon = document.querySelector('#menu-bars')
+    menuIcon.classList.toggle('fa-bars', toggle)
+    menuIcon.classList.toggle('fa-times', !toggle)
+  }
+
+  return (
+    <header className={styles.header}>
+      <a href='#' className={styles.header__logo}>
+        <span className='fas fa-utensils'></span>resto.
+      </a>
+      <nav
+        className={classNames(styles.header__nav, {
+          [styles['show-menu']]: toggle
+        })}
+      >
+        <a
+          href='#home'
+          className={classNames(
+            styles['header__nav-link'],
+            styles['active-link']
+          )}
+        >
+          home
+        </a>
+        <a href='#dishes' className={styles['header__nav-link']}>
+          dishes
+        </a>
+        <a href='#about' className={styles['header__nav-link']}>
+          about
+        </a>
+        <a href='#menu' className={styles['header__nav-link']}>
+          menu
+        </a>
+        <a href='#reviews' className={styles['header__nav-link']}>
+          reviews
+        </a>
+        <a href='#order' className={styles['header__nav-link']}>
+          order
+        </a>
+      </nav>
+      <div className={styles.header__icons}>
+        <span
+          id='menu-bars'
+          className={classNames('fas fa-bars', styles.menu)}
+          onClick={onMenuClick}
+        />
+        <span
+          id='search-icon'
+          className={classNames('fas fa-search', styles.search)}
+        />
+        <span className='fas fa-heart' />
+        <span className='fas fa-shopping-cart' />
+      </div>
+    </header>
+  )
 }
 
 export default Header
