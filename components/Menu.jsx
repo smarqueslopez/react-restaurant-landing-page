174bbfec228db023045bb940770ecf2d85fc4b53
smarqueslopez <smarqueslopez@gmail.com>
2021-10-07 23:36:43 +0200

    fix(global): :ambulance: Fixing no escaped entities in component

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
   return (
     <section className={styles.menu} id='menu'>
       <h3 className='sub-heading'>our menu</h3>
-      <h1 className='heading'>today's speciality</h1>
+      <h1 className='heading'>today&apos;s speciality</h1>
       <div className={styles.menu__container}>
         {menuDishList.map((menu, index) => {
           return <Dish key={index} {...menu} />
