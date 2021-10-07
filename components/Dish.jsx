d9636ff202bf4e147f3252669ac5d9f4c99d0d7a
smarqueslopez <smarqueslopez@gmail.com>
2021-10-07 23:13:27 +0200

    feat(global): :sparkles: Adding description to Dish component

--- components/Dish.jsx
+++ components/Dish.jsx
@@ -1,20 +1,27 @@
 import Stars from './StarRating'
-import styles from '@/styles/components/Dish.module.scss'
 import classNames from 'classnames'
+import styles from '@/styles/components/Dish.module.scss'
 
-function Dish({ title, img, rating, price }) {
+function Dish({ title, img, rating, price, description, favorited, detailed }) {
   return (
     <div className={styles.dish}>
-      <a
-        href='#'
-        className={classNames('fas', 'fa-heart', styles.dish__favorite)}
-      ></a>
-      <a
-        href='#'
-        className={classNames('fas', 'fa-eye', styles.dish__detail)}
-      ></a>
+      {favorited ? (
+        <a
+          href='#'
+          className={classNames('fas', 'fa-heart', styles.dish__favorite)}
+        ></a>
+      ) : null}
+      {detailed ? (
+        <a
+          href='#'
+          className={classNames('fas', 'fa-eye', styles.dish__detail)}
+        ></a>
+      ) : null}
       <img className={styles.dish__image} src={img} alt={title} />
       <h3 className={styles.dish__title}>{title}</h3>
+      {description ? (
+        <p className={styles.dish__description}>{description}</p>
+      ) : null}
       <Stars value={rating} />
       <span className={styles.dish__price}>{`$${price}`}</span>
       <a href='#' className='button'>
