fb9de4f2f66ca20912bc972a305b1032eee7fed3
smarqueslopez <smarqueslopez@gmail.com>
2021-10-07 23:21:42 +0200

    fix(global): :bug: Fixing correct title in section component

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -5,8 +5,8 @@ import styles from '@/styles/components/Menu.module.scss'
 function Menu() {
   return (
     <section className={styles.menu} id='menu'>
-      <h3 className='sub-heading'>our dishes</h3>
-      <h1 className='heading'>popular dishes</h1>
+      <h3 className='sub-heading'>our menu</h3>
+      <h1 className='heading'>today's speciality</h1>
       <div className={styles.menu__container}>
         {menuDishList.map((menu, index) => {
           return <Dish key={index} {...menu} />
