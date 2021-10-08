1470677824bffadb8c60b0998708bdfa356a9c27
smarqueslopez <smarqueslopez@gmail.com>
2021-10-08 17:27:54 +0200

    feat(global): :sparkles: Adding loader component

---
+++ components/Loader.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import styles from '@/styles/components/Loader.module.scss'
+
+function Loader() {
+  useEffect(() => {
+    window.onload = () =>
+      setInterval(() => {
+        document
+          .querySelector([styles.loader])
+          .classList.add(styles['fade-out'])
+      }, 3000)
+  }, [])
+
+  return (
+    <div className={styles.loader}>
+      <img src='images/loader.gif' alt='loader' />
+    </div>
+  )
+}
+
+export default Loader
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '@/components/Header'
 import Main from '@/components/Main'
 import Footer from '@/components/Footer'
 import styles from '@/styles/pages/Home.module.scss'
+import Loader from '@/components/loader'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Header />
       <Main />
       <Footer />
+      <Loader />
     </div>
   )
 }
