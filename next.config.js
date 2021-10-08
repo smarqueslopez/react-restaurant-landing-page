17e2ebad547b400be357dc12cc862d356d96b0d6
smarqueslopez <smarqueslopez@gmail.com>
2021-10-08 13:15:47 +0200

    build(global): :wrench: Adding webpack config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jp(e*)g|svg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'images/[hash]-[name].[ext]',
+            publicPath: `/_next/static/images/`,
+            outputPath: 'static/images'
+          }
+        }
+      ]
+    })
+
+    return config
+  }
 }
