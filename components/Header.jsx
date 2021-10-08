d87e7b5aafa4ebb11b8db3de175ee2e420c73530
smarqueslopez <smarqueslopez@gmail.com>
2021-10-08 17:07:31 +0200

    feat(global): :sparkles: Adding scrollSpy

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,11 +9,30 @@ function Header() {
   const [toggleSearch, setToggleSearch] = useState(false)
 
   useEffect(() => {
+    const menuIcon = document.querySelector('#menu-bars')
+    const sectionList = document.querySelectorAll('section')
+    const navLinkList = document.querySelectorAll('header nav a')
     window.onscroll = () => {
-      const menuIcon = document.querySelector('#menu-bars')
       menuIcon.classList.add('fa-bars')
       menuIcon.classList.remove('fa-times')
       setToggleMenu(false)
+      sectionList.forEach((section) => {
+        const id = section.getAttribute('id')
+        const menuItem = document.querySelector(`header nav a[href="#${id}"]`)
+        if (menuItem) {
+          const scrollY = window.scrollY
+          const sectionTop = section.offsetTop - 150
+          const activeLinkClass = classNames(styles['active-link'])
+          if (
+            scrollY > sectionTop &&
+            scrollY <= sectionTop + section.offsetHeight
+          ) {
+            menuItem.classList.add(activeLinkClass)
+          } else {
+            menuItem.classList.remove(activeLinkClass)
+          }
+        }
+      })
     }
   }, [])
 
