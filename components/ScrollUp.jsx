8112522467d62c82f0f1b0e85cc1e897db4df2dc
smarqueslopez <smarqueslopez@gmail.com>
2021-10-08 17:49:16 +0200

    feat(global): :sparkles: Addiing scroll up component

---
+++ components/ScrollUp.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import classNames from 'classnames'
+import style from '@/styles/components/ScrollUp.module.scss'
+
+function ScrollUp() {
+  useEffect(() => {
+    const scrollUp = document.getElementById('scroll-up')
+    window.onscroll = () => {
+      window.scrollY >= 200
+        ? scrollUp.classList.add(style['scroll-up--show'])
+        : scrollUp.classList.remove(style['scroll-up--show'])
+    }
+  }, [])
+
+  return (
+    <a href='#' className={style['scroll-up']} id='scroll-up' title={'Go up!'}>
+      <i
+        className={classNames('fas', 'fa-arrow-up', style['scroll-up__icon'])}
+      ></i>
+    </a>
+  )
+}
+
+export default ScrollUp
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '@/components/Header'
 import Main from '@/components/Main'
 import Footer from '@/components/Footer'
 import Loader from '@/components/Loader'
+import ScrollUp from '@/components/ScrollUp'
 import styles from '@/styles/pages/Home.module.scss'
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
       <Main />
       <Footer />
       <Loader />
+      <ScrollUp />
     </div>
   )
 }
